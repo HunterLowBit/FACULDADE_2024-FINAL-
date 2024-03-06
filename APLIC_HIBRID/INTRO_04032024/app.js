94d14c8e4d2fbaa3356063723ae9c8e47aa1328d
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-06 18:53:54 -0300

    Aula 2

--- APLIC_HIBRID/INTRO_04032024/app.js
+++ APLIC_HIBRID/INTRO_04032024/app.js
@@ -3,9 +3,27 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! GET')
+})
+
+app.post('/post', (req, res) => {
+  res.send('Hello World!  POST')
+})
+
+app.put('/put', (req, res) => {
+  res.send('Hello World! PUT')
+})
+
+app.delete('/delete', (req, res) => {
+  res.send('Hello World! DELETE')
+})
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} !`)
 })
+
+// Path: APLIC_HIBRID/INTRO_04032024/package.json   
