3d9f31edc08321075331c912c9939950588a7803
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-11 17:59:17 -0300

    ok

--- APLIC_HIBRID/INTRO_04032024/app2(REPO).js
+++ APLIC_HIBRID/INTRO_04032024/app2(REPO).js
@@ -5,6 +5,11 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World! GET')
 });
+
+app.get('/random.text', (req, res) => {
+    res.send('random.text')
+});
+
 app.listen(port, () => {
     console.log(`Execução no ${port} !`)
 });
