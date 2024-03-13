415eb7ef0ab4c66d9d19602ebaa063fdf027471a
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-13 18:49:00 -0300

    ok

--- APLIC_HIBRID/INTRO_04032024/index.js
+++ APLIC_HIBRID/INTRO_04032024/index.js
@@ -3,9 +3,15 @@ const routes = require('./routes');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // para leitura de dados via POST
+app.use(express.json());
 
 app.use('/api', routes);
+
+router.post('/exemplo', (req, res) => {
+    const {body} = req.body;
+    res.json({message: `Recebido ${body}`});
+    console.log(`Recebido ${body}`);
+});
 
 app.listen(port, () => {
     console.log(`Rodando na porta ${port} !`)
