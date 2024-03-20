4890205a817db26335f9866dcdab47597ef6b639
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-20 18:47:43 -0300

    OK

--- APLIC_HIBRID/INTRO_04032024/REPO_REF/index copy.js
+++ APLIC_HIBRID/INTRO_04032024/REPO_REF/index copy.js
@@ -6,17 +6,13 @@ const port = 3000;
 
 app.use(express.json());
 
-mongoose
-  .connect("mongodb://localhost:27017/aula", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Conectado ao MongoDB");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+app.use("/api", routes);
+
+router.post("/exemplo", (req, res) => {
+  const { body } = req.body;
+  res.json({ message: `Recebido ${body}` });
+  console.log(`Recebido ${body}`);
+});
 
 app.listen(port, () => {
   console.log(`Rodando na porta ${port} !`);
