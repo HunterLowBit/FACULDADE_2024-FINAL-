ba340920506ab471d387302a155372d70bf6f4c2
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-20 18:33:25 -0300

    up

--- APLIC_HIBRID/INTRO_04032024/controllers/userController.js
+++ APLIC_HIBRID/INTRO_04032024/controllers/userController.js
@@ -1,21 +1,40 @@
-const User = require('../models/Users');
+const User = require("../models/Users");
 
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(400).json({ message: error.message });   
-    }
-}
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 exports.createUser = async (req, res) => {
-    const user = req.body;
-    
-    try {
-        const savedUser = await User.create(user);
-        res.status(200).json(savedUser);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-}
+  const user = req.body;
+
+  try {
+    const savedUser = await User.create(user);
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+exports.updateUser = async (req, res) => {
+  try {
+    const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "usuario deletado com sucesso" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
