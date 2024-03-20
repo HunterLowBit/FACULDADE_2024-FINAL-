b629060d3bd6f0449bf8cc183e0c029630463ab6
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-20 18:41:07 -0300

    UP

--- APLIC_HIBRID/INTRO_04032024/routes/userRoutes.js
+++ APLIC_HIBRID/INTRO_04032024/routes/userRoutes.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+
+router.get("/", userController.getAllUsers);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
