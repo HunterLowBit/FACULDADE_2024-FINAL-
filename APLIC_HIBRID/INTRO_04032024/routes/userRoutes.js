52ee277574560fd297e327fe3ef4c17e05c325e6
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-20 18:41:52 -0300

    up

--- APLIC_HIBRID/INTRO_04032024/routes/userRoutes.js
+++ APLIC_HIBRID/INTRO_04032024/routes/userRoutes.js
@@ -6,3 +6,5 @@ router.get("/", userController.getAllUsers);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
+
+module.exports = router;
