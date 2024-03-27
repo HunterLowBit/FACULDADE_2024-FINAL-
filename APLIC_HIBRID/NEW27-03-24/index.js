a1d31c09b40f2c511c3d46644607f8fb05eeffb5
HLB_FACULDADE <HunterLowBit@Faculdade.com>
2024-03-27 18:29:51 -0300

    Rodando Node

--- APLIC_HIBRID/NEW27-03-24/index.js
+++ APLIC_HIBRID/NEW27-03-24/index.js
@@ -3,16 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!, http://localhost:3000/users/VALOR/books/VALOR");
+    console.log("Hello World!, pagina inicial");
 });
 
 app.get("/users/:userId/books/:bookId", (req, res) => {
     res.send(req.params);
     const { userId, bookId } = req.params;
     console.log(`userId: ${userId}, bookId: ${bookId}`);
-    console.log(' o usuario e o livro ${userId}, ${bookId} foram selecionados');
+    console.log(`O usuário ${userId} e o livro ${bookId}, foram selecionados`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Rodando app no endereço http://localhost:${port}`);
 });
