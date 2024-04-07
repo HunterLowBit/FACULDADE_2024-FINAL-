1da8178af96c9d3d2daadbb7f8b05676d0aabe23
HunterLowBit_Win11VsCode <HunterLowBit@users.noreply.github.com>
2024-04-07 20:10:23 -0300

    a

--- APLIC_HIBRID/01042024/src/models/User.js
+++ APLIC_HIBRID/01042024/src/models/User.js
@@ -1,13 +1,22 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  nome: String,
-  email: String,
-  senha: String,
-  admin: Boolean,
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
 
+const mongoose = require("mongoose");
+class User {
+  constructor({ nome, email, senha, admin, created_at, updated_at }) {
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.admin = admin;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
+}
+
+const userSchema = new mongoose.Schema(
+  { nome: String, email: String, senha: String, admin: Boolean, created_at: Date, updated_at: Date },
+  { timestamps: true }
+);
+
+userSchema.loadClass(User);
 
 module.exports = mongoose.model("User", userSchema);
 // Path: src/models/User.js
