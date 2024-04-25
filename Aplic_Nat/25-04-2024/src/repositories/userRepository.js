88d9f47935f872409c4493220a9d79377d369984
HunterLowBit_Win11VsCode <HunterLowBit@users.noreply.github.com>
2024-04-25 19:11:54 -0300

    AP_NAT

---
+++ Aplic_Nat/25-04-2024/src/repositories/userRepository.js
@@ -0,0 +1,26 @@
+class UserRepository {
+  async create(userData) {
+    const user = new User(userData);
+    return user.save();
+  }
+
+  async findAll() {
+    return User.find();
+  }
+
+  async findById(id) {
+    return User.findById(id);
+  }
+
+  async updateById(id, userData) {
+    return User.findByIdAndUpdate(id, userData, { new: true, runValidators: true });
+  }
+
+  async deleteById(id) {
+    return User.findByIdAndDelete(id);
+  }
+}
+
+module.exports = new UserRepository();
+
+// Path: src/repositories/userRepository.js
